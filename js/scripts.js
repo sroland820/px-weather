5592bb07e138772faccb4a64ff558c55b2ae6d54
Sammie <sroland@engr-134-07.eastern.ewu.edu>
2015-03-11 14:06:32 -0700

    v?

--- js/scripts.js
+++ js/scripts.js
@@ -55,5 +55,43 @@ var loadWeather = function(location) {
 
 /* DO NOT DELETE ANYTHING ABOVE THIS LINE */
 
+/ REF: http://foundation.zurb.com/docs/
+// REF: http://simpleweatherjs.com/
+
+  // 2. _simpleWeather()_ object
+  $.simpleWeather({
+    
+    location: '77586', // change zip
+    unit: 'f',
+    
+    // Get _weather_ object
+    success: function(weather) {
+      
+      // Get & store temperature
+      var temp = weather.temp;
+      // Get & store city
+      var city = weather.city;
+      // Get & store state
+      var state = weather.region;
+      // Get & store thumbnail
+      var thumb = weather.thumbnail;
+      console.log(thumb);
+      
+      // Output to hooks in HTML
+      $('.temp').text(temp);
+      $('.city').text(city);
+      $('.state').text(state);
+      $('.thumb img').attr('src', thumb);
+      
+      // See console for _weather_ object
+      console.log(weather);
+    },
+  
+    // if error
+    error: function(error) {  
+      $('body').html('<p>' + error + '</p>');
+    }
+  
+  });
 
 
