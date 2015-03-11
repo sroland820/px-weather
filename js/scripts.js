0899a23685627fe496a3b1b24173ca60cc4aacee
Sammie <sroland@engr-134-07.eastern.ewu.edu>
2015-03-11 12:15:05 -0700

    v2

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,68 @@
-var cheney = '99004';
-var spokane = '99216';
+// 1. Check for Geolocation
+// REF: http://codepen.io/fleeting/pen/Idsaj
+// Browser support geolocation?  
+  if (navigator.geolocation) {
+    // Yes! Show button
+    $('.getGeolocation').show(); 
+  } else {
+    // No. Hide button
+    $('.getGeolocation').hide();
+  }
 
+// 2. Get Geolocation & return Simple Weather
+$('.getGeolocation').on('click', function() {
+  
+    navigator.geolocation.getCurrentPosition(function(position) {
+    //load weather using your lat/lng coordinates. See _loadWeather()_ below
+    loadWeather(position.coords.latitude+','+position.coords.longitude); 
+    // See latitute & longitude. Note, wait a few seconds
+    console.log(position.coords.latitude+','+position.coords.longitude);
+  });
+  
+});
 
- $.simpleWeather({
+// 3. Wrap SimpleWeather in a function called _loadWeather()
+var loadWeather = function(location) {
     
-    location: '77586', // change zip
-    unit: 'f',
+    $.simpleWeather({
+    location: location,
     
     // Get _weather_ object
     success: function(weather) {
+        
+      console.log(weather);
       
       // Get & store temperature
       var temp = weather.temp;
       // Get & store city
       var city = weather.city;
-      // Get & store state
-      var state = weather.region;
-			// Get & store thumbnail
-      var thumb = weather.thumbnail;
-      console.log(thumb);
       
       // Output to hooks in HTML
       $('.temp').text(temp);
       $('.city').text(city);
-      $('.state').text(state);
-      $('.thumb img').attr('src', thumb);
       
       // See console for _weather_ object
       console.log(weather);
-    },
-  
-    // if error
-    error: function(error) {  
-      $('body').html('<p>' + error + '</p>');
     }
   
   });
+    
+}; // end of _loadWeather()_ function
+
+/**
+ * requestAnimationFrame
+ */
+window.requestAnimationFrame = (function(){
+    return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            window.oRequestAnimationFrame      ||
+            window.msRequestAnimationFrame     ||
+            function (callback) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+})();
+
+/* DO NOT DELETE ANYTHING ABOVE THIS LINE */
 
 
