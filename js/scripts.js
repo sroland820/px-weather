729de75c4025cafde6cae5052214564d88caad66
Sammie <sroland820@eagles.ewu.edu>
2015-03-15 21:01:10 -0700

    v4

--- js/scripts.js
+++ js/scripts.js
@@ -1,99 +1,37 @@
-// REF: http://foundation.zurb.com/docs/
-// REF: http://simpleweatherjs.com/
+ $.backstretch("weather.jpg");
 
-// 1. Check for Geolocation
-// REF: http://codepen.io/fleeting/pen/Idsaj
-// Browser support geolocation?  
-  if (navigator.geolocation) {
-    // Yes! Show button
-    $('.getGeolocation').show(); 
-  } else {
-    // No. Hide button
-    $('.getGeolocation').hide();
-  }
 
-// 2. Get Geolocation & return Simple Weather
-$('.getGeolocation').on('click', function() {
+// click button, capture zip field
+$('#getZip').click(function() {
   
-    navigator.geolocation.getCurrentPosition(function(position) {
-    //load weather using your lat/lng coordinates. See _loadWeather()_ below
-    loadWeather(position.coords.latitude+','+position.coords.longitude); 
-    // See latitute & longitude. Note, wait a few seconds
-    console.log(position.coords.latitude+','+position.coords.longitude);
-  });
+  // stop default, <form> from submitting data
+  event.preventDefault();
   
-});
-
-// 3. Wrap SimpleWeather in a function called _loadWeather()
-var loadWeather = function(location) {
-    
-    $.simpleWeather({
-    location: location,
-    
-    // Get _weather_ object
-    success: function(weather) {
-    		
-      console.log(weather);
-      
-      // Get & store temperature
-      var temp = weather.temp;
-      // Get & store city
-      var city = weather.city;
-      
-      // Output to hooks in HTML
-      $('.temp').text(temp);
-      $('.city').text(city);
-      
-      // See console for _weather_ object
-      console.log(weather);
-    }
+  // get and store data
+  var zip = $('#zip').val();
+  console.log(zip);  
   
-  });
-    
-}; // end of _loadWeather()_ function
-
-
-/* DO NOT DELETE ANYTHING ABOVE THIS LINE */
-
-/ REF: http://foundation.zurb.com/docs/
-// REF: http://simpleweatherjs.com/
-
-  // 2. _simpleWeather()_ object
+  // Get Weather
   $.simpleWeather({
     
-    location: '77586', // change zip
-    unit: 'f',
-    
-    // Get _weather_ object
+    location: zip,  
     success: function(weather) {
       
-      // Get & store temperature
-      var temp = weather.temp;
-      // Get & store city
-      var city = weather.city;
-      // Get & store state
-      var state = weather.region;
-      // Get & store thumbnail
-      var thumb = weather.thumbnail;
-      console.log(thumb);
-      
-      // Output to hooks in HTML
-      $('.temp').text(temp);
-      $('.city').text(city);
-      $('.state').text(state);
-      $('.thumb img').attr('src', thumb);
-      
-      // See console for _weather_ object
+      // get weather OK? 
       console.log(weather);
-    },
-  
-    // if error
-    error: function(error) {  
+
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city);
+      $('.country').text(weather.country);
+    
+    },  
+    error: function(error) {
       $('body').html('<p>' + error + '</p>');
     }
   
   });
-/*DO NOT DELETE ABOVE THIS */
-
- $.backstretch("weather.jpg");
-
+  
+  // clear zip
+  $('#getWeather').val('');
+  
+});
